6ecef295498fc8c789f2de0638f23636af6cc6f5
111110100 <elomibao@gmail.com>
2021-05-08 09:28:37 +0800

    Added second solution

--- leetcode/maxProduct.py
+++ leetcode/maxProduct.py
@@ -5,5 +5,10 @@ class Solution:
         l = sorted(nums)
         return (l[-1]-1) * (l[-2]-1)
 
-answer = Solution()
+class Solution2:
+    def maxProduct(self, nums: List[int]) -> int:
+        m, n = nums.pop(nums.index(max(nums)))-1, max(nums)-1
+        return m * n
+
+answer = Solution2()
 print(answer.maxProduct([3,4,5,2]))
