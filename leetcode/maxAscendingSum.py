b59236dcf5907b3273c5d927e9f550d4f65e4c5c
111110100 <elomibao@gmail.com>
2021-05-10 00:03:40 +0800

    Moved checkIfPangram.py, maxAscendingSum.py to right folder

---
+++ leetcode/maxAscendingSum.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def maxAscendingSum(self, nums:List[int]) -> int:
+        maxSum = nums[0]
+        total = 0
+        index = 1
+        return maxSum
+
+answer = Solution()
+print(answer.maxAscendingSum([10,20,30,5,10,50]))
